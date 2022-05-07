d39cb2aa525f6183fe360af17357c0e7dd0f8e2d
danillistov <danillistov@gmail.com>
2022-05-07 08:45:30 +0300

    feat(moon): added white nosie

--- src/plugins/threejs/moon/index.js
+++ src/plugins/threejs/moon/index.js
@@ -3,11 +3,20 @@ import * as THREE from 'three';
 import createAtmosphereObject from './objects/atmosphere';
 import createSphereObject from './objects/sphere';
 
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+
+
+import noiseVertexShader from '@/static/shaders/noiseVertex.glsl';
+import noiseFragmentShader from '@/static/shaders/noiseFragment.glsl';
+
 export function initSphere(elem) {
     const scene = new THREE.Scene();
+
     scene.background = new THREE.Color(0x0d0d0d);
 
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth  / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth  / window.innerHeight, 0.1, 1000);
 
     const renderer = new THREE.WebGLRenderer({
         canvas: elem,
@@ -17,18 +26,41 @@ export function initSphere(elem) {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 
-    renderer.render(scene, camera);
-
     camera.position.setZ(25);
+
+    // Composer
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+
+    let counter = 0.0;
+    const effect = {
+        uniforms: {
+            'tDiffuse': {
+                value: null,
+            },
+            'amount': {
+                value: counter,
+            },
+        },
+        vertexShader: noiseVertexShader,
+        fragmentShader: noiseFragmentShader,
+    };
+
+    const customPass = new ShaderPass(effect);
+    customPass.renderToScreen = false;
+    composer.addPass(customPass);
+
     const group = new THREE.Group();
 
     scene.add(group);
 
     const sphere = createSphereObject(THREE);
+
     group.add(sphere);
 
     const atmosphere = createAtmosphereObject(THREE);
-    atmosphere.scale.set(1.1, 1.1, 1.1);
+    atmosphere.scale.set(1.01, 1.01, 1.01);
     group.add(atmosphere);
 
     const mouse = {
@@ -37,7 +69,8 @@ export function initSphere(elem) {
     };
 
     function animate() {
-        requestAnimationFrame(animate);
+        counter += 0.01;
+        customPass.uniforms["amount"].value = counter;
 
         sphere.rotation.y += 0.001;
 
@@ -47,7 +80,9 @@ export function initSphere(elem) {
             duration: 2,
         });
 
-        renderer.render(scene, camera);
+        composer.render();
+
+        requestAnimationFrame(animate);
     }
 
     animate();
@@ -57,3 +92,4 @@ export function initSphere(elem) {
         mouse.y = -(event.clientY / innerHeight) * 2 + 1;
     });
 }
+
--- src/static/shaders/fragment.glsl
+++ src/static/shaders/fragment.glsl
@@ -4,7 +4,7 @@ varying vec2 vertexUV;
 varying vec3 vertexNormal;
 
 void main() {
-    float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
+    float intensity = 1.0 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
 
     vec3 atmosphere = vec3(1.0, 1.0, 1.0) * pow(intensity, 1.5);
 
