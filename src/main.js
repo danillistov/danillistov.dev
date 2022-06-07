b4f94fb431f849b5bfc4e1e51b372fd6391078ac
danillistov <danillistov@gmail.com>
2022-06-07 09:35:28 +0300

    feat(global): add colot theme ti global variable

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue';
 
 import router from './router';
 
-createApp(App)
-    .use(router)
-    .mount('#app');
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+app.config.globalProperties.$colorTheme = {
+    black: '#1C1D21',
+    grey: '#31353D',
+    darkBlue: '#445878',
+    lightBlue: '#92CDCF',
+    white: '#EEEFF7',
+    success: '#00B000',
+    error: '#E74C3C',
+};
+
+console.log(app);
+
