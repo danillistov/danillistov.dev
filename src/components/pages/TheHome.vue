35ed80a46e224d6a9244978438de6193ad488328
danil <danil>
2022-08-07 20:57:00 +0900

    feat(TheHome): change TheHome component

--- src/components/pages/TheHome.vue
+++ src/components/pages/TheHome.vue
@@ -1,26 +1,14 @@
 <template>
     <div class="home">
         <div class="main-title">
-            <TypingTextAnimation
-                v-if="false"
-                text="Title typing animation!"
-                :speed="1"
-                :color="$colorTheme.black"
-                class="main-title__text"
-            />
+            
         </div>
     </div>
 </template>
 
 <script>
-import TypingTextAnimation from '@/components/common/TypingTextAnimation.vue';
-
 export default {
     name: "Home",
-
-    components: {
-        TypingTextAnimation,
-    },
 };
 </script>
 
@@ -28,12 +16,5 @@ export default {
     .home {
         width: 100%;
         height: 100%;
-    }
-
-    .main-title {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
     }
 </style>
