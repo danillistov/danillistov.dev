3bfe7ee2f19f2e71d50c7a8dab835ab97d930916
danil <danil>
2023-01-17 19:15:33 +0900

    refactor(router): delete unused routes from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/components/pages/TheHome.vue';
-// import About from '@/components/pages/TheAbout.vue';
-// import Contacts from '@/components/pages/TheContacts.vue';
 
 const PAGE_NAMES = new Map();
 PAGE_NAMES.set('Home', 'Danil Listov - Vue Frontend Developer');
@@ -15,16 +13,6 @@ const router = createRouter({
             name: 'Home',
             component: Home,
         },
-        // {
-        //     path: '/about',
-        //     name: 'About',
-        //     component: About,
-        // },
-        // {
-        //     path: '/contacts',
-        //     name: 'Contacts',
-        //     component: Contacts,
-        // },
         {
             path: '/:pathMatch(.*)*',
             name: 'Home',
